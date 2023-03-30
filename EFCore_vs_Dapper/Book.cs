565655c8bf299df158ae1ea45467f93fd78a2bfc
NathanCarlsson <nathan.carlsson@slimmingworld.co.uk>
2023-03-30 08:55:35 +0100

    Add project files.

---
+++ EFCore_vs_Dapper/Book.cs
@@ -0,0 +1,9 @@
+﻿namespace EFCore_vs_Dapper
+{
+    public class Book
+    {
+        public long Id { get; set; }
+        public string Title { get; set; }
+        public string Author { get; set; }
+    }
+}
